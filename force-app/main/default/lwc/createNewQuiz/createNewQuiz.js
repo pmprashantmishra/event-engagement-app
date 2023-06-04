b92edc65779e700f35821d0057ae41bb6fc6780a
Prashant Mishra <prashant.mishra@salesforce.com>
2023-06-04 12:34:57 +0530

    backend process done

--- force-app/main/default/lwc/createNewQuiz/createNewQuiz.js
+++ force-app/main/default/lwc/createNewQuiz/createNewQuiz.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 import getEventNamesWithIds from '@salesforce/apex/EventAppCtrl.getEventNamesWithIds';
+import createQuiz from '@salesforce/apex/EventAppCtrl.createQuiz';
 export default class CreateNewQuiz extends LightningElement {
     eventOptions = [];
     questionWithOptionsList = [];
@@ -18,13 +19,19 @@ export default class CreateNewQuiz extends LightningElement {
     }
 
     handleAddRow() {
+        // Question Row
         let questionRow = {
             questionText : '',
-            options : [],
+            options : {
+                option1 : '',
+                option2 : '',
+                option3 : '',
+                option4 : ''
+            },
             correctAnswer : '',
             marks : ''
         }
-        this.questionWithOptionsList = [...this.questionWithOptionsList, Object.create(questionRow)];
+        this.questionWithOptionsList = this.questionWithOptionsList.concat([questionRow]);
     }
 
     handleEventChange(event) {
@@ -41,7 +48,19 @@ export default class CreateNewQuiz extends LightningElement {
     }
 
     handleOption1Change(event) {
-        this.questionWithOptionsList[event.currentTarget.dataset.index].questionText = event.target.value;
+        this.questionWithOptionsList[event.currentTarget.dataset.index].options.option1 = event.target.value;
+    }
+
+    handleOption2Change(event) {
+        this.questionWithOptionsList[event.currentTarget.dataset.index].options.option2 = event.target.value;
+    }
+
+    handleOption3Change(event) {
+        this.questionWithOptionsList[event.currentTarget.dataset.index].options.option3 = event.target.value;
+    }
+
+    handleOption4Change(event) {
+        this.questionWithOptionsList[event.currentTarget.dataset.index].options.option4 = event.target.value;
     }
 
     handleAnswerChange(event) {
@@ -50,6 +69,19 @@ export default class CreateNewQuiz extends LightningElement {
 
     handleMarksChange(event) {
         this.questionWithOptionsList[event.currentTarget.dataset.index].marks = event.target.value;
-        console.log(`${JSON.stringify(this.questionWithOptionsList)}`);
+    }
+
+    handleAddRowClick() {
+        this.handleAddRow();
+    }
+
+    handleCreateQuizClick(){
+        createQuiz({eventId : this.eventId, quizName : this.quizName, quizQuestions : JSON.stringify(this.questionWithOptionsList)})
+            .then(result => {
+                console.log('result ', result);
+            })
+            .catch(error => {
+                console.log('error creating quiz ', error);
+            })
     }
 }
